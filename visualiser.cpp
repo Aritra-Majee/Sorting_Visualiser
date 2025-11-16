d7d54065ef251cfb80b9228a75c14592250eb1ae
Aritra-Majee <aritramajee19@gmail.com>
2025-11-16 19:09:20 +0000

    Refactor countingSort

--- visualiser.cpp
+++ visualiser.cpp
@@ -486,32 +486,25 @@ void countSort(vector<int>& arr, sf::RenderWindow& window)
 
 void countingSort(vector<int>& arr, int exp, sf::RenderWindow& window)
 {
-    vector<int> output(arr.size());
-    vector<int> count(10, 0);
-
+    vector<int> buckets[10];
+    
     for (int i = 0; i < arr.size(); ++i)
     {
-        count[(arr[i] / exp) % 10]++;
+        int digit = (arr[i] / exp) % 10;   
+        buckets[digit].push_back(arr[i]);
     }
-
-    for (int i = 1; i < 10; ++i)
+   
+    int idx = 0;
+    for (int d = 0; d < 10; ++d)
     {
-        count[i] += count[i - 1];
-    }
+        for (int val : buckets[d])
+        {
+            arr[idx] = val;
+            drawBars(window, arr, idx + 1, idx - 1);
+            sf::sleep(sf::milliseconds(SPEED));
 
-    for (int i = arr.size() - 1; i >= 0; --i)
-    {
-        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-        count[(arr[i] / exp) % 10]--;
-    }
-
-    for (int i = 0; i < arr.size(); ++i)
-    {
-        arr[i] = output[i];
-
-        drawBars(window, arr,i+1, i-1);
-        sf::sleep(sf::milliseconds(SPEED));  
-
+            idx++;
+        }
     }
 }
 
